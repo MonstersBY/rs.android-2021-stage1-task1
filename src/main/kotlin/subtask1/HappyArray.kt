a444ea437ed48d9ad38ab3164813f6e344204a34
Alex Romanchenko <monsters.alex0@gmail.com>
2021-05-29 22:31:23 +0300

    init: HappyArray, BillCounter

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -2,8 +2,16 @@ package subtask1
 
 class HappyArray {
 
-    // TODO: Complete the following function
+
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        var sadArray = sadArray.toMutableList()
+        var id = 0
+        while (id < sadArray.size - 1 ){
+            if (id != 0 && sadArray[id-1] + sadArray[id+1] < sadArray[id]){
+                sadArray.removeAt(id)
+                id--
+            }else id++
+        }
+        return sadArray.toIntArray()
     }
 }
